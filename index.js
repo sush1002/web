d909300adbcc6b2432692fa6bb262c75bac9ae1a
sush1002 <msushmitha1002@gmail.com>
2023-07-18 21:07:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+
+//document.getElementById("bat").addEventListener("click",openForm);
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+ // document.getElementById("bat").addEventListener("click",closeForm);
+function togglehide(){
+  document.getElementById("mytoggle").style.display="block";
+}
